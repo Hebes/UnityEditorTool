15d7f31b3b4f4f668085565ce9c84ec24351d19e
Hebes <1607388033@qq.com>
2023-06-11 19:06:07 +0800

    更新

---
+++ Child/13.UI界面/ACUIInterface.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using UnityEditor;
+using UnityEngine;
+
+namespace ACTool
+{
+    public class ACUIInterface : EditorWindow
+    {
+        private Vector2 scrollRoot;
+        private int gridId;
+        private UnityEngine.Color m_color;
+        private GUIContent colorTitle = new GUIContent("颜色选择");
+
+        [MenuItem("Assets/暗沉EditorTool/UI界面")]//#E
+        public static void GeneratorFindComponentTool()
+        {
+            GetWindow(typeof(ACUIInterface), false, "UI界面").Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, position.width - 20, position.height - 20));
+            {
+                EditorGUILayout.BeginHorizontal(GUILayout.Width(200));
+                {
+                    EditorGUILayout.BeginVertical(new GUIStyle("LODBlackBox"), GUILayout.Width(200));
+                    {
+                        gridId = GUILayout.SelectionGrid(gridId, new[] { "1", "2", "3" }, 1, "ButtonMid", GUILayout.Width(180),GUILayout.Height(100));
+                    }
+                    EditorGUILayout.EndVertical();
+                }
+                EditorGUILayout.EndHorizontal();
+                
+            }
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(300, 20, position.width - 20, position.height - 20));
+            {
+                EditorGUILayout.BeginHorizontal(GUILayout.Width(200));
+                {
+                    EditorGUILayout.BeginVertical(new GUIStyle("LODBlackBox"));
+                    {
+                        m_color = EditorGUILayout.ColorField(colorTitle, m_color, true, true, true);
+                    }
+                    EditorGUILayout.EndVertical();
+                }
+                EditorGUILayout.EndHorizontal();
+
+            }
+            GUILayout.EndArea();
+        }
+    }
+}
