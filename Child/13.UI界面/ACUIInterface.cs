67f3dd4c686f3cd2a41ea646c321ea3f8da549b2
Hebes <1607388033@qq.com>
2023-06-11 23:21:26 +0800

    更新

--- Child/13.UI界面/ACUIInterface.cs
+++ Child/13.UI界面/ACUIInterface.cs
@@ -10,6 +10,14 @@ namespace ACTool
         private int gridId;
         private UnityEngine.Color m_color;
         private GUIContent colorTitle = new GUIContent("颜色选择");
+        private float vSbarValue;
+        private Vector2 ttt;
+
+        // The variable to control where the scrollview 'looks' into its child elements.
+        Vector2 scrollPosition;
+
+        // The string to display inside the scrollview. 2 buttons below add &amp; clear this string.
+        string longString = "This is a long-ish string";
 
         [MenuItem("Assets/暗沉EditorTool/UI界面")]//#E
         public static void GeneratorFindComponentTool()
@@ -19,32 +27,126 @@ namespace ACTool
 
         private void OnGUI()
         {
-            GUILayout.BeginArea(new Rect(10, 10, position.width - 20, position.height - 20));
-            {
-                EditorGUILayout.BeginHorizontal(GUILayout.Width(200));
-                {
-                    EditorGUILayout.BeginVertical(new GUIStyle("LODBlackBox"), GUILayout.Width(200));
-                    {
-                        gridId = GUILayout.SelectionGrid(gridId, new[] { "1", "2", "3" }, 1, "ButtonMid", GUILayout.Width(180),GUILayout.Height(100));
-                    }
-                    EditorGUILayout.EndVertical();
-                }
-                EditorGUILayout.EndHorizontal();
-                
-            }
-            GUILayout.EndArea();
-            GUILayout.BeginArea(new Rect(300, 20, position.width - 20, position.height - 20));
-            {
-                EditorGUILayout.BeginHorizontal(GUILayout.Width(200));
-                {
-                    EditorGUILayout.BeginVertical(new GUIStyle("LODBlackBox"));
-                    {
-                        m_color = EditorGUILayout.ColorField(colorTitle, m_color, true, true, true);
-                    }
-                    EditorGUILayout.EndVertical();
-                }
-                EditorGUILayout.EndHorizontal();
+            //scrollPosition = GUILayout.BeginScrollView(
+            //scrollPosition, GUILayout.Width(100), GUILayout.Height(100));
 
+            //// We just add a single label to go inside the scroll view. Note how the
+            //// scrollbars will work correctly with wordwrap.
+            //GUILayout.Label(longString);
+
+            //// Add a button to clear the string. This is inside the scroll area, so it
+            //// will be scrolled as well. Note how the button becomes narrower to make room
+            //// for the vertical scrollbar
+            //if (GUILayout.Button("Clear"))
+            //    longString = "";
+            // if (GUILayout.Button("Clear"))
+            //    longString = "";
+            // if (GUILayout.Button("Clear"))
+            //    longString = "";
+            // if (GUILayout.Button("Clear"))
+            //    longString = "";
+            // if (GUILayout.Button("Clear"))
+            //    longString = "";
+            // if (GUILayout.Button("Clear"))
+            //    longString = "";
+
+            //// End the scrollview we began above.
+            //GUILayout.EndScrollView();
+
+            //// Now we add a button outside the scrollview - this will be shown below
+            //// the scrolling area.
+            //if (GUILayout.Button("Add More Text"))
+            //    longString += "\nHere is another line";
+
+            GUILayout.BeginArea(new Rect(5, 5, position.width - 10, position.height - 10), new GUIStyle("LODBlackBox"));
+            {
+                GUILayout.Space(5);
+                EditorGUILayout.BeginHorizontal();
+                {
+                    //vSbarValue = GUILayout.VerticalScrollbar(vSbarValue, 700f, 700f, 100.0f);
+                    GUILayout.Space(5);
+                    scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, "Wizard Box", GUILayout.Height(position.height - 20));
+                    {
+                        //gridId = GUILayout.SelectionGrid(gridId, new[] { "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "311", "2", "3", "1", "2", "312" }, 1, GUILayout.Height(position.height - 25));
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                        if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                         if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                         if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                         if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                         if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                         if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+                         if (GUILayout.Button("测hi是", GUILayout.Height(50)))
+                        {
+
+                        }
+
+                    }
+                    EditorGUILayout.EndScrollView();
+
+                    //EditorGUILayout.BeginVertical("Wizard Box", GUILayout.Height(position.height - 20), GUILayout.Width(200));
+                    //{
+                    //    gridId = GUILayout.SelectionGrid(gridId, new[] { "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "311" }, 1, GUILayout.Height(position.height-25));
+                    //}
+                    //EditorGUILayout.EndVertical(); 
+
+                    GUILayout.Space(5);
+                    EditorGUILayout.BeginHorizontal("Wizard Box", GUILayout.Width(position.width - 200), GUILayout.Height(position.height - 20));
+                    {
+                        EditorGUILayout.BeginVertical();
+                        {
+                            m_color = EditorGUILayout.ColorField(colorTitle, m_color, true, true, true);
+                        }
+                        EditorGUILayout.EndVertical();
+                    }
+                    EditorGUILayout.EndHorizontal();
+                    GUILayout.Space(5);
+                }
+                EditorGUILayout.EndHorizontal();
             }
             GUILayout.EndArea();
         }
--- Child/13.校验丢失/ttt.cs
+++ Child/13.校验丢失/ttt.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
